b994131551cce284d02726b7fd7d640e1b0adb86
johanstokking <johan@stokking.nl>
2016-09-27 12:25:53 +0200

    Adapted to production environment

--- examples/simple.js
+++ examples/simple.js
@@ -3,15 +3,15 @@
 const ttnazureiot = require('..');
 
 // Replace with your AppEUI and App Access Key
-const appEUI = '<insert AppEUI>';
-const appAccessKey = '<insert App Access Key>';
+const appId = '<insert app ID>';
+const accessKey = '<insert access key>';
 
 // Replace with your Azure IoT Hub name, key name and key
 const hubName = '<insert hub name>';
-const keyName = '<insert key name, e.g. iothubowner>';
+const keyName = '<insert key name>';
 const key = '<insert key>';
 
-const bridge = new ttnazureiot.Bridge(appEUI, appAccessKey, hubName, keyName, key);
+const bridge = new ttnazureiot.Bridge(appId, accessKey, hubName, keyName, key);
 
 bridge.on('ttn-connect', () => {
   console.log('TTN connected');
@@ -21,6 +21,6 @@ bridge.on('error', err => {
   console.warn('Error', err);
 });
 
-bridge.on('uplink', data => {
-  console.log('Uplink', data);
+bridge.on('message', data => {
+  console.log('Message', data);
 });
--- index.js
+++ index.js
@@ -8,41 +8,41 @@ const amqp = require('azure-iot-device-amqp');
 const common = require('azure-iot-common');
 const EventEmitter = require('events');
 
-const DEFAULT_TTN_BROKER = 'staging.thethingsnetwork.org';
+const DEFAULT_TTN_REGION = 'eu';
 const SAK_CONNECTION_STRING = 'HostName=%s.azure-devices.net;SharedAccessKeyName=%s;SharedAccessKey=%s';
 const DEVICE_CONNECTION_STRING = 'HostName=%s.azure-devices.net;DeviceId=%%s;SharedAccessKey=%%s';
 
 const Bridge = class Bridge extends EventEmitter {
-  constructor(appEUI, appAccessKey, hubName, keyName, key, options) {
+  constructor(appId, accessKey, hubName, keyName, key, options) {
     super();
     options = options || {};
 
-    this._createMessage = options.createMessage || function(uplink) {
+    this._createMessage = options.createMessage || function(deviceId, message) {
       const metadata = {
-        deviceId: uplink.devEUI,
-        time: uplink.metadata.server_time
+        deviceId: deviceId,
+        time: message.metadata.time
       };
-      return Object.assign({}, uplink.fields, metadata);
+      return Object.assign({}, message.payload_fields, metadata);
     }
 
     this.registry = iothub.Registry.fromConnectionString(util.format(SAK_CONNECTION_STRING, hubName, keyName, key));
     this.deviceConnectionString = util.format(DEVICE_CONNECTION_STRING, hubName);
     this.devices = {};
 
-    this.ttnClient = new ttn.Client(options.ttnBroker || DEFAULT_TTN_BROKER, appEUI, appAccessKey);
+    this.ttnClient = new ttn.Client(options.region || DEFAULT_TTN_REGION, appId, accessKey);
     this.ttnClient.on('connect', super.emit.bind(this, 'ttn-connect'));
     this.ttnClient.on('error', super.emit.bind(this, 'error'));
-    this.ttnClient.on('uplink', this._handleUplink.bind(this));
+    this.ttnClient.on('message', this._handleMessage.bind(this));
   }
 
-  _getDevice(devEUI) {
-    if (this.devices[devEUI]) {
-      return Promise.resolve(this.devices[devEUI]);
+  _getDevice(deviceId) {
+    if (this.devices[deviceId]) {
+      return Promise.resolve(this.devices[deviceId]);
     }
 
     return new Promise((resolve, reject) => {
       const device = new iothub.Device(null);
-      device.deviceId = devEUI;
+      device.deviceId = deviceId;
       this.registry.create(device, (err, deviceInfo) => {
         if (!err) {
           resolve(deviceInfo);
@@ -59,14 +59,14 @@ const Bridge = class Bridge extends EventEmitter {
       });
     }).then(deviceInfo => {
       const key = deviceInfo.authentication.SymmetricKey.primaryKey;
-      const connectionString = util.format(this.deviceConnectionString, devEUI, key);
+      const connectionString = util.format(this.deviceConnectionString, deviceId, key);
       const client = amqp.clientFromConnectionString(connectionString);
       return new Promise((resolve, reject) => {
         client.open(err => {
           if (err) {
             reject(err);
           } else {
-            this.devices[devEUI] = client;
+            this.devices[deviceId] = client;
             resolve(client);
           }
         });
@@ -74,27 +74,28 @@ const Bridge = class Bridge extends EventEmitter {
     });
   }
 
-  _handleUplink(uplink) {
-    console.log('%s: Handling uplink', uplink.devEUI);
+  _handleMessage(deviceId, data) {
+    console.log(data);
+    console.log('%s: Handling message', deviceId);
 
-    this._getDevice(uplink.devEUI).then(deviceInfo => {
-      const message = JSON.stringify(this._createMessage(uplink));
+    this._getDevice(deviceId).then(deviceInfo => {
+      const message = JSON.stringify(this._createMessage(deviceId, data));
 
       deviceInfo.sendEvent(new device.Message(message), (err, res) => {
         if (err) {
-          console.warn('%s: Could not send event: %s. Closing connection', uplink.devEUI, err);
+          console.warn('%s: Could not send event: %s. Closing connection', deviceId, err);
           deviceInfo.close(err => {
             // Delete reference even if close failed
-            delete this.devices[uplink.devEUI];
+            delete this.devices[deviceId];
           });
           this.emit('error', err);
         } else {
-          this.emit('uplink', { devEUI: uplink.devEUI, message: message });
+          this.emit('message', { deviceId: deviceId, message: message });
         }
       });
     })
     .catch(err => {
-      console.log('%s: Could not get device: %s', uplink.devEUI, err);
+      console.log('%s: Could not get device: %s', deviceId, err);
       super.emit('error', err);
     });
   }
