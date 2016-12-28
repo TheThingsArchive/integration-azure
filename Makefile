c37f10ae13d67530388bef3fd89909e54f3d28bb
Antoine Rondelet <rondelet.antoine@gmail.com>
2016-12-28 19:24:13 +0100

    Changed tag and environment variables' prefix

--- Makefile
+++ Makefile
@@ -2,10 +2,8 @@ SHELL = bash
 
 .PHONY: all deps docker
 
-all: docker
-
 deps:
 	npm install
 
 docker: deps
-	docker build -t thethingsindustries/azure-integration-process .
+	docker build -t thethingsnetwork/azure-integration-process .
--- bridge.js
+++ bridge.js
@@ -8,9 +8,9 @@ const accessKey = process.env.TTN_APP_ACCESS_KEY;
 const region = process.env.TTN_REGION;
 
 // Azure related settings
-const hubName = process.env.TTI_AZURE_HUBNAME;
-const keyName = process.env.TTI_AZURE_KEYNAME;
-const key = process.env.TTI_AZURE_KEY;
+const hubName = process.env.TTN_AZURE_HUBNAME;
+const keyName = process.env.TTN_AZURE_KEYNAME;
+const key = process.env.TTN_AZURE_KEY;
 
 const bridge = new ttnazureiot.Bridge(region, appId, accessKey, hubName, keyName, key);
 
